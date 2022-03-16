5add19f80935a9e105ff0538b9a4720872134936
Andrew Smithson <smithson@uk.ibm.com>
2022-03-16 10:51:44 +0000

    Add other OAS3 repos to group

--- scripts/git.js
+++ scripts/git.js
@@ -114,9 +114,10 @@ jQuery.fn.loadRepositories = function (username) {
 
         //iterate over repos array and remove oas3 entries into another array
         repositories.forEach(function(repo) {
-            // console.log(repo); //print to console
+            console.log(repo); //print to console
 
-            if(repo.id === 429458443 || repo.id === 465738025) { // Db2 sample unique ID = 429458443
+            if(repo.id === 429458443 || repo.id === 465738025
+                || repo.id === 419718267 || repo.id === 445501328) { // Db2 sample unique ID = 429458443
                 oas3repos.push(repo); //remove openapi 3 repos from the array received
                 return;
             }
